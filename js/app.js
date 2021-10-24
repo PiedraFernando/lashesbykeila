41e0a182dfc601949548b758e56d68b00d7e2105
Fernando Piedra <fernandopiedram@gmail.com>
2021-10-24 03:00:57 -0500

    agregacion de animaciones

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const imgs = document.getElementsByTagName('img')
+const aboutme = document.getElementById('aboutme')
+const cargarImg = (entradas, observador) => {
+    entradas.forEach(entrada => {
+        if(entrada.isIntersecting){
+            entrada.target.classList.add('visible')
+        }else{
+            entrada.target.classList.remove('visible')
+        }
+    });
+}
+const observador = new IntersectionObserver(cargarImg, {
+    root:null,
+    rootMargin: '0px',
+    threshold: 0.6,
+})
+for(const img of imgs){
+    observador.observe(img)
+}
+observador.observe(aboutme)
